fb4b8faf6b6ebef341e41628864383dd870a2f7d
Alex Rodgers <alex.e.rodgers@gmail.com>
2016-01-18 14:45:47 -0800

    Added command DrivetrainInitialize and edited Gearbox to add a retract and extend method.

--- src/org/usfirst/frc/team3189/robot/subsystems/Gearbox.java
+++ src/org/usfirst/frc/team3189/robot/subsystems/Gearbox.java
@@ -6,18 +6,25 @@ import org.usfirst.frc.team3189.robot.utils.Piston;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class Gearbox extends Subsystem {
-	
+
 	private Piston piston = new Piston(RobotMap.gearboxRetract, RobotMap.gearboxExtend);
-    // Put methods for controlling this subsystem
-    // here. Call these from Commands.
-	
-	public void togglePistonState(){
+	// Put methods for controlling this subsystem
+	// here. Call these from Commands.
+
+	public void togglePistonState() {
 		piston.toggle();
 	}
-	
-    public void initDefaultCommand() {
-        // Set the default command for a subsystem here.
-        //setDefaultCommand(new MySpecialCommand());
-    }
-}
 
+	public void extendPiston() {
+		piston.extendPiston();
+	}
+
+	public void retractPiston() {
+		piston.retractPiston();
+	}
+
+	public void initDefaultCommand() {
+		// Set the default command for a subsystem here.
+		// setDefaultCommand(new MySpecialCommand());
+	}
+}

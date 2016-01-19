37762f387a925720e57db574034171158b223647
Alex Rodgers <alex.e.rodgers@gmail.com>
2016-01-18 17:22:33 -0800

    Added more java documentation to more Commands

--- src/org/usfirst/frc/team3189/robot/commands/CompressorStart.java
+++ src/org/usfirst/frc/team3189/robot/commands/CompressorStart.java
@@ -5,38 +5,51 @@ import org.usfirst.frc.team3189.robot.Robot;
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
+ * 
+ * @author Alex, Mitch, Nate
  *
  */
 public class CompressorStart extends Command {
+	/**
+	 * Uses compressor subsystem
+	 */
+	public CompressorStart() {
+		requires(Robot.Compressor);
+		// Use requires() here to declare subsystem dependencies
+		// eg. requires(chassis);
+	}
 
-    public CompressorStart() {
-    	requires(Robot.Compressor);
-        // Use requires() here to declare subsystem dependencies
-        // eg. requires(chassis);
-    }
+	// Called just before this Command runs the first time
+	/**
+	 * Starts the compressor, only initialized, not continuous
+	 */
+	protected void initialize() {
+		Robot.Compressor.start();
+	}
 
-    // Called just before this Command runs the first time
-    protected void initialize() {
-    	Robot.Compressor.start();
-    }
+	// Called repeatedly when this Command is scheduled to run
+	protected void execute() {
+	}
 
-    // Called repeatedly when this Command is scheduled to run
-    protected void execute() {
-    }
+	// Make this return true when this Command no longer needs to run execute()
+	protected boolean isFinished() {
+		return false;
+	}
 
-    // Make this return true when this Command no longer needs to run execute()
-    protected boolean isFinished() {
-        return false;
-    }
+	// Called once after isFinished returns true
+	/**
+	 * Stops the compressor
+	 */
+	protected void end() {
+		Robot.Compressor.stop();
+	}
 
-    // Called once after isFinished returns true
-    protected void end() {
-    	Robot.Compressor.stop();
-    }
-
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
-    protected void interrupted() {
-    	Robot.Compressor.stop();
-    }
+	// Called when another command which requires one or more of the same
+	// subsystems is scheduled to run
+	/**
+	 * Stops the compressor if another command occurs
+	 */
+	protected void interrupted() {
+		Robot.Compressor.stop();
+	}
 }
--- src/org/usfirst/frc/team3189/robot/commands/DrivetrainControl.java
+++ src/org/usfirst/frc/team3189/robot/commands/DrivetrainControl.java
@@ -11,7 +11,9 @@ import edu.wpi.first.wpilibj.command.Command;
  *
  */
 public class DrivetrainControl extends Command {
-
+	/**
+	 * Accesses drivetrain located in robot
+	 */
 	public DrivetrainControl() {
 		requires(Robot.drivetrain);
 
@@ -20,6 +22,10 @@ public class DrivetrainControl extends Command {
 	protected void initialize() {
 	}
 
+	/**
+	 * Always gets the input from both joysticks to set the speed of the left
+	 * and right side of the robot, controlling the speed
+	 */
 	protected void execute() {
 		Robot.drivetrain.setspeed(Robot.oi.getLeftJoystickY(), Robot.oi.getRightJoystickY());
 	}
@@ -31,6 +37,9 @@ public class DrivetrainControl extends Command {
 	protected void end() {
 	}
 
+	/**
+	 * Sets the speed to 0 in case another command needs to use the robot
+	 */
 	protected void interrupted() {
 		Robot.drivetrain.setspeed(0, 0);
 	}
